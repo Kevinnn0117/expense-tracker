94207871d5884c368e64454f4b98b790e2c9ac05
Kevin Wang <kevinnn0117@gmail.com>
2026-09-02 19:20:24 -0400

    feat: add ExpenseManager with ArrayList

---
+++ src/ExpenseManager.java
@@ -0,0 +1,31 @@
+import java.util.ArrayList;
+
+public class ExpenseManager {
+    private ArrayList<Expense> expenses = new ArrayList<>();
+
+    public void add(Expense expense) {
+        expenses.add(expense);
+    }
+
+    public void list() {
+        for (int i = 0; i < expenses.size(); i++) {
+            System.out.print(
+                    expenses.get(i).getDescription() + " | " +
+                            expenses.get(i).getCategory() + " | "
+            );
+
+            System.out.printf("%.2f%n", expenses.get(i).getAmount());
+        }
+    }
+
+    public double total() {
+        double total = 0;
+
+        for (int i = 0; i < expenses.size(); i++) {
+            total += expenses.get(i).getAmount();
+        }
+
+        return total;
+    }
+
+}
