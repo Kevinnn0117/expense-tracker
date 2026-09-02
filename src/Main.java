5b08ffbccc68a77b4e5f45c6e49466099b2f99d9
Kevin Wang <kevinnn0117@gmail.com>
2026-09-02 19:23:16 -0400

    refactor: use ExpenseManager in Main

--- src/Main.java
+++ src/Main.java
@@ -1,23 +1,13 @@
 public class Main {
     public static void main(String[] args) {
-        Expense e1 = new Expense(12.50, "Lunch", "Food");
-        Expense e2 = new Expense(2.75, "Metro", "Travel");
-        Expense e3 = new Expense(18.00, "Book", "Learning");
+        ExpenseManager manager = new ExpenseManager();
 
-        Expense[] expenses = {e1, e2, e3};
+        manager.add(new Expense(12.50, "Lunch", "Food"));
+        manager.add(new Expense(2.75, "Metro", "Travel"));
+        manager.add(new Expense(18.00, "Book", "Learning"));
 
-        for (int i = 0; i < expenses.length; i++) {
-            System.out.print(
-                expenses[i].getDescription() + " | " +
-                        expenses[i].getCategory() + " | "
-            );
+        manager.list();
 
-            System.out.printf("%.2f%n", expenses[i].getAmount());
-        }
-
-
-        double total = e1.getAmount() + e2.getAmount() + e3.getAmount();
-
-        System.out.println("TOTAL: " + total);
+        System.out.printf("TOTAL: %.2f%n", manager.total());
     }
 }
